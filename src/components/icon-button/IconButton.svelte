ad1e14ba41e7e60118f21118201f608c84da7f2a
PierreElmen <pierre.elmen@gmail.com>
2025-01-30 23:41:24 +0100

    done with layout

---
+++ src/components/icon-button/IconButton.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import Icon from '../icon-component/Icon.svelte';
+
+	export let icon: string;
+	export let size: string = '24px';
+	export let type: 'primary' | 'secondary' | 'cancel' = 'primary';
+	export let transparent: boolean = false;
+	export let small: boolean = false;
+	const dispatch = createEventDispatcher();
+
+	function handleClick() {
+		dispatch('click');
+	}
+</script>
+
+<button
+	on:click={handleClick}
+	class={`flex cursor-pointer items-center justify-center rounded-full shadow-lg transition-all duration-200
+        ${type === 'primary' ? 'bg-black text-white hover:bg-gray-800' : ''}
+        ${type === 'secondary' ? 'bg-gray-600 text-white hover:bg-gray-500' : ''}
+        ${type === 'cancel' ? 'bg-red-600 text-white hover:bg-red-500' : ''}
+        ${transparent ? 'bg-transparent text-gray-400 shadow-none hover:text-white' : ''}
+        ${transparent && type === 'cancel' ? 'hover:text-red-500' : ''}
+        h-12 w-12
+        ${small ? 'h-8 w-8' : ''}
+        focus:outline-none active:translate-y-1 active:scale-95
+        active:shadow-sm`}
+>
+	<Icon {icon} {size} />
+</button>
---
+++ src/components/ui/popupWrapper/PopupWrapper.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import { createEventDispatcher, onDestroy, onMount } from 'svelte';
+	import IconButton from '../../icon-button/IconButton.svelte';
+	import Icon from '../../icon-component/Icon.svelte';
+
+	export let header: string = 'Popup';
+	export let width: string = 'fit-content';
+	export let height: string = 'fit-content';
+
+	export let icon: string;
+
+	const dispatch = createEventDispatcher();
+
+	function onClose() {
+		dispatch('close');
+	}
+
+	function handleKeyDown(event: KeyboardEvent) {
+		if (event.key === 'Escape') {
+			onClose();
+		}
+	}
+
+	onMount(() => {
+		window.addEventListener('keydown', handleKeyDown);
+	});
+
+	onDestroy(() => {
+		window.removeEventListener('keydown', handleKeyDown);
+	});
+</script>
+
+<div
+	class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+	role="backdrop"
+	on:click={onClose}
+	on:keydown={handleKeyDown}
+>
+	<div
+		class="modal-content overflow-hidden rounded-lg bg-white shadow-lg transition-all"
+		on:click|stopPropagation
+		style="width: {width}; height: {height};"
+	>
+		<div class="p-4">
+			<div class="header flex items-center justify-between border-b-2 pb-2">
+				<div class="flex items-center gap-2">
+					{#if icon}
+						<div class="bg-text flex h-7 w-7 items-center justify-center rounded-full text-white">
+							<Icon {icon} size="14px" />
+						</div>
+					{/if}
+					<h2 class="text-text text-3xl font-semibold text-black" id="popup-title">{header}</h2>
+				</div>
+				<IconButton on:click={onClose} size="18px" icon="Close" transparent />
+			</div>
+			<div class="popup-scroll max-h-[80vh] overflow-y-scroll p-4">
+				<slot />
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.modal-content {
+		max-width: 90vw;
+	}
+
+	.popup-scroll {
+		scrollbar-width: thin;
+		scrollbar-color: darkgray transparent;
+	}
+
+	.popup-scroll::-webkit-scrollbar {
+		width: 8px;
+	}
+
+	.popup-scroll::-webkit-scrollbar-track {
+		background: black;
+	}
+
+	.popup-scroll::-webkit-scrollbar-thumb {
+		background-color: darkgray;
+		border-radius: 4px;
+		transition: background-color 0.2s ease;
+	}
+
+	.popup-scroll::-webkit-scrollbar-thumb:hover {
+		background-color: #a9a9a9;
+	}
+</style>
